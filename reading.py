01e2f420d7bd207de565456f7d173e4357bc57ce
clagodmor <clagodmor@gmail.com>
2018-06-16 11:18:30 +0200

    Adecuo lectura a nuevos datos y añado generación de fichero

--- reading.py
+++ reading.py
@@ -11,7 +11,7 @@ import zipfile
 
 
 # Extracción de ficheros
-zipfilename = "data/DATOS_HIST.zip"
+zipfilename = "data/HISTORICAL_DATA.zip"
 password = None
 
 # open and extract all files
@@ -26,32 +26,34 @@ z.close()
 #lectura y ordenación por fechas
 
 # WIND POWER DATA
-wind_data="data/EOLICA_HIST.csv"
-photo_data="data/FOTO_HIST.csv"
-demand_data="data/DEMANDA_HIST.csv"
-price_data="data/PRECIO_HIST.csv"
-solarthermal_data="data/TERMO_HIST.csv"
+wind_data="data/WIND_HIST.csv"
+photo_data="data/PHOTO_HIST.csv"
+demand_data="data/DEMAND_HIST.csv"
+price_data="data/PRICE_HIST.csv"
+solarthermal_data="data/THERMO_HIST.csv"
 temperature_data="data/TEMP_HIST.csv"
 
-df4 = pd.read_table(price_data,sep=';')
-df4=df4.rename(columns={'FECHA':'date','PRECIO cEUR/kWh':'price'})
+df= pd.read_table(price_data,sep=';')
+df=df.rename(columns={'FECHA':'date','PRECIO cEUR/kWh':'price'})
 
 df2 = pd.read_table(wind_data,sep=';')
-df2=df2.rename(columns={'FECHA':'date','EOLICA kW':'wind'})
+df2=df2.rename(columns={'FECHA':'date','EOLICA':'wind'})
 
 df3 = pd.read_table(demand_data,sep=';')
-df3=df3.rename(columns={'FECHA':'date','DEMANDA kW':'demand'})
+df3=df3.rename(columns={'FECHA':'date','DEMANDA':'demand'})
 
-df1 = pd.read_table(photo_data,sep=';')
-df1=df1.rename(columns={'FECHA':'date','FOTOVOLTAICA kW':'photo'})
+df4 = pd.read_table(photo_data,sep=';')
+df4=df4.rename(columns={'FECHA':'date','FOTOVOLTAICA':'photo'})
 
 df5 = pd.read_table(temperature_data,sep=';')
 df5=df5.rename(columns={'FECHA':'date','TEMPERATURA dK':'temperature'})
 
+df6 = pd.read_table(temperature_data,sep=';')
+df6=df6.rename(columns={'FECHA':'date','TERMOSOLAR':'thermo'})
 
-df1=df1.merge(df2,left_on='date',right_on='date',how='outer')
-df1=df1.merge(df3,left_on='date',right_on='date',how='outer')
-df1=df1.merge(df4,left_on='date',right_on='date',how='inner')
-df1=df1.merge(df5,left_on='date',right_on='date',how='outer')
+df=df.merge(df2,left_on='date',right_on='date',how='inner')
+df=df.merge(df3,left_on='date',right_on='date',how='inner')
+df=df.merge(df4,left_on='date',right_on='date',how='inner')
+df=df.merge(df5,left_on='date',right_on='date',how='inner')
 
-print(df1)
+df.to_csv('out.csv')
