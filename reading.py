b604f4d95ceed6367fa93d8051b178b0f851fb50
clagodmor <clagodmor@gmail.com>
2018-06-23 23:36:21 +0200

    ñado producción temsolar y redondeos

--- reading.py
+++ reading.py
@@ -32,9 +32,11 @@ demand_data="data/DEMAND_HIST.csv"
 price_data="data/PRICE_HIST.csv"
 solarthermal_data="data/THERMO_HIST.csv"
 temperature_data="data/TEMP_HIST.csv"
+hydro_data="data/DISP_HIDRO_HIST.csv"
 
 df= pd.read_table(price_data,sep=';')
 df=df.rename(columns={'FECHA':'date','PRECIO cEUR/kWh':'price'})
+df=df.round(3)
 
 df2 = pd.read_table(wind_data,sep=';')
 df2=df2.rename(columns={'FECHA':'date','EOLICA':'wind'})
@@ -46,14 +48,20 @@ df4 = pd.read_table(photo_data,sep=';')
 df4=df4.rename(columns={'FECHA':'date','FOTOVOLTAICA':'photo'})
 
 df5 = pd.read_table(temperature_data,sep=';')
-df5=df5.rename(columns={'FECHA':'date','TEMPERATURA dK':'temperature'})
+df5=df5.rename(columns={'FECHA':'date','TEMPERATURA dK':'temp'})
+df5=df5.round(0)
 
-df6 = pd.read_table(temperature_data,sep=';')
+df6 = pd.read_table(solarthermal_data,sep=';')
 df6=df6.rename(columns={'FECHA':'date','TERMOSOLAR':'thermo'})
+
+df7 = pd.read_table(hydro_data,sep=';')
+df7=df7.rename(columns={'FECHA':'date','DISP_HIDRO kW':'hydro'})
 
 df=df.merge(df2,left_on='date',right_on='date',how='inner')
 df=df.merge(df3,left_on='date',right_on='date',how='inner')
 df=df.merge(df4,left_on='date',right_on='date',how='inner')
 df=df.merge(df5,left_on='date',right_on='date',how='inner')
+df=df.merge(df6,left_on='date',right_on='date',how='inner')
+df=df.merge(df7,left_on='date',right_on='date',how='inner')
 
-df.to_csv('out.csv')
+df.to_csv('data/out.csv',index=False,sep=';')
